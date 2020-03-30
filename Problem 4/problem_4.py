9fcda3ebcdf23f6d41673fe81cc2b8ca746ae262
Pablo Rey Pedrosa <pablo.reypedrosa@gmail.com>
2020-03-30 22:38:26 +0200

    Give a bit of format to README and add an idea for the solution.

---
+++ Problem 4/problem_4.py
@@ -0,0 +1,3 @@
+# IDEA: Have an upper bound and a lowe bound, and update it with every item in the array.
+# At the end, if the lower bound isn't in the array, that's the first integer missing. 
+# If it is in the array, the upper bound is the integer missing instead. 
