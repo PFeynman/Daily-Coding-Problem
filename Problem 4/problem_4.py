055c46cee41e288f9bcd96ff7e21453a4ce24943
Pablo Rey Pedrosa <pablo.reypedrosa@gmail.com>
2020-04-01 21:50:26 +0200

    Problem 4 solution. Closes #4

--- Problem 4/problem_4.py
+++ Problem 4/problem_4.py
@@ -1,3 +1,26 @@
-# IDEA: Have an upper bound and a lowe bound, and update it with every item in the array.
-# At the end, if the lower bound isn't in the array, that's the first integer missing. 
-# If it is in the array, the upper bound is the integer missing instead. 
+# My idea made no sense (I wrote it before going to sleep, without thinking a lot). 
+# After much thinking, I looked for the solution in geeksforgeeks.org, understood it and implemented it.
+
+def separate(array):
+  j = 0
+  for i in range(len(array)):
+    if array[i] <= 0:
+      array[j], array[i] = array[i], array[j]
+      j += 1
+    
+  return j
+
+def find_first_positive(array):
+  pivot = separate(array)
+  size = len(array) - pivot
+  array = array[pivot:]
+
+  for i in range(size): 
+    if (abs(array[i]) - 1 < size and array[abs(array[i]) - 1] > 0): 
+      array[abs(array[i]) - 1] = -array[abs(array[i]) - 1] 
+
+  for i in range(size): 
+    if (array[i] > 0):
+      return i + 1
+
+  return size + 1
--- Problem 4/test_problem_4.py
+++ Problem 4/test_problem_4.py
@@ -1,2 +1,15 @@
-import unittest
+import unittest
+
+from problem_4 import find_first_positive
+
 class TestsProblem4(unittest.TestCase):
+  def setUp(self):
+    self.input_array  = [[3, 4, -1, 1], [1, 2, 0], [-1, 5, 2, -4, 1, 4, 3]]
+    self.output_array = [2, 3, 6]
+  
+  def test_find_first_positive(self):
+    for pair in zip(self.input_array, self.output_array):
+      self.assertEqual(find_first_positive(pair[0]), pair[1])
+
+if __name__ == "__main__":
+    unittest.main()
